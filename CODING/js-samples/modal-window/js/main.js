152b45f43ba90aecfc384025da5bf714b00d089c
Kris Van Looy <kris.van.looy@gmail.com>
2019-01-20 22:00:31 +0100

    js-samples - modal window with vanilla CSS and JavaScript

--- CODING/js-samples/modal-window/js/main.js
+++ CODING/js-samples/modal-window/js/main.js
@@ -1,2 +1,14 @@
-// START EDITING YOUR JAVASCRIPT HERE
-// ===============
+// Select the modal button
+
+const modalButton = document.querySelector('.jsModalOpen')
+const modalCloseButton = document.querySelector('.jsModalClose')
+
+// Add an event listener to the modal button
+modalButton.addEventListener('click', _ => {
+    document.body.classList.add('modal-is-open')
+})
+
+// Add an event listener to the modal close icon
+modalCloseButton.addEventListener('click', _ => {
+    document.body.classList.remove('modal-is-open')
+})
