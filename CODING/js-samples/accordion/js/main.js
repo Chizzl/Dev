cf975b53f7676828fc9786da768876526d13bf21
Kris Van Looy <kris.van.looy@gmail.com>
2019-01-28 21:43:02 +0100

    accordion functionality

--- CODING/js-samples/accordion/js/main.js
+++ CODING/js-samples/accordion/js/main.js
@@ -1,2 +1,11 @@
-// START EDITING YOUR JAVASCRIPT HERE
-// ===============
+// select all accordions
+const accordions = document.querySelectorAll('.accordion')
+
+// loop over the accordions and add an event listener on the headers
+accordions.forEach(accordion => {
+    const accordionHeader = accordion.querySelector('.accordion__header')
+
+    accordionHeader.addEventListener('click', e => {
+        accordion.classList.toggle('is-open')
+    })
+})
